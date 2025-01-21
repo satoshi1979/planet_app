4a66178f51fc6554532f3a9b42446db3ad669a10
satoshi1979 <satoshi19791212@gmail.com>
2025-01-21 21:56:15 +0900

    惑星情報管理アプリ課題のPlanetモデル作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PlanetController;
+use App\Models\Planet;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('planets', PlanetController::class);
